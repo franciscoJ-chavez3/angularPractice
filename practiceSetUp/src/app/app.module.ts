c5c1db9ac81078ca5b5a0cdb67dc6090aedd3a4b
franciscoJ-chavez3 <fjchavez@outlook.com>
2020-01-13 21:21:16 -0800

    generated components

--- practiceSetUp/src/app/app.module.ts
+++ practiceSetUp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { FaqsComponent } from './pages/faqs/faqs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    FaqsComponent
   ],
   imports: [
     BrowserModule,
