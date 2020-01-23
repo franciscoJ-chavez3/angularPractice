b69a779d8f1c97af327fbf5dc73b850992fbb067
franciscoJ-chavez3 <fjchavez@outlook.com>
2020-01-23 00:46:13 -0800

    homePage

--- webStore/src/app/app.module.ts
+++ webStore/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
