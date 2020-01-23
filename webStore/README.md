8dce6778a6d4fc1e137ec1ec41588f57a12eda79
franciscoJ-chavez3 <fjchavez@outlook.com>
2020-01-22 21:30:42 -0800

    webStore

--- webStore/README.md
+++ webStore/README.md
@@ -1,4 +1,4 @@
-# PracticeSetUp
+# WebStore
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.22.
 
--- webStore/src/app/app.component.spec.ts
+++ webStore/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'practiceSetUp'`, () => {
+  it(`should have as title 'webStore'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('practiceSetUp');
+    expect(app.title).toEqual('webStore');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('practiceSetUp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('webStore app is running!');
   });
 });
